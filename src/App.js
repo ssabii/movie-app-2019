2ef568b5967d4d20c7a30f05e3d610c5c4bd67a4
ssabii <fhy1930@gmail.com>
2021-12-04 23:46:15 +0900

    All you need to know about State

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    counter: 0,
+    count: 0,
   };
 
   add = () => {
-    console.log("add");
+    // 이 방식으로 상태를 변경하는 방법은 추천하지 않는다.
+    // this.setState({ count: this.state.count + 1 });
+    this.setState((current) => ({ count: current.count + 1 }));
   };
 
   minus = () => {
-    console.log("minus");
+    this.setState({ count: this.state.count - 1 });
   };
 
   render() {
     return (
       <div>
-        <h1>The number is: {this.state.counter}</h1>
+        <h1>The number is: {this.state.count}</h1>
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
