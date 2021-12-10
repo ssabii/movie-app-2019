1564ed72fc55d4a5a18a4dc92953a2b0699d4c1e
ssabii <fhy1930@gmail.com>
2021-12-11 01:27:18 +0900

    Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <div>something</div>;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
