eb1dbf223deb87a3e8f95b41933775502450dc04
ssabii <fhy1930@gmail.com>
2021-12-11 01:36:18 +0900

    Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navigation from "./components/Navigation";
 import Home from "./routes/Home";
 import About from "./routes/About";
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
